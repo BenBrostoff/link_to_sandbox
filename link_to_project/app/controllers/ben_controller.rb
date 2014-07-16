6af1ba5c80be28dc0615a16fa6f57058d95f52a6
Ben Brostoff <ben.brostoff@gmail.com>
2014-07-15 22:21:21 -0400

    Working on adding method:delete on linked_to

--- link_to_project/app/controllers/ben_controller.rb
+++ link_to_project/app/controllers/ben_controller.rb
@@ -12,5 +12,12 @@ class BenController < ApplicationController
   def example
     @example = params[:id]
   end
-  
+
+  def destroy
+    @adjective = Adjective.find(params[:id])
+    @adjective.destroy
+    redirect_to :root
+  end
+
+
 end
--- link_to_project/config/routes.rb
+++ link_to_project/config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get '/ben/about' => 'ben#about', :as => :about
   get '/ben/contact' => 'ben#contact', :as => :contact
   get '/ben/:id' => 'ben#example', :as => :example
+  delete '/ben/:id' => 'ben#destroy', :as => :destroy
 
 end

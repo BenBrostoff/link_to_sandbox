bbd5ee2ab85310c38e94d72edaa33d63185d9482
Ben Brostoff <ben.brostoff@gmail.com>
2014-07-15 23:45:55 -0400

    if_and_unless pages moved

--- link_to_project/config/routes.rb
+++ link_to_project/config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
   get '/ben/about' => 'ben#about', :as => :about
   get '/ben/contact' => 'ben#contact', :as => :contact
 
+  get '/ben/if_and_unless' =>'ben#if_and_unless', :as => :if_and_unless
+
   get '/ben/adjectives' => 'ben#adjectives', :as => :adjectives
   post '/ben/adjectives' => 'ben#create'
-  
+
   get '/ben/:id' => 'ben#example', :as => :example
   delete '/ben/:id' => 'ben#destroy', :as => :destroy
 
